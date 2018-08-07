c5e05976c7ca8985d1e7be44f6720dd4a274b240
William Yeung <william@real.co>
2018-08-07 15:33:51 +0800

    update layout

--- SKPhotoBrowser/SKActionView.swift
+++ SKPhotoBrowser/SKActionView.swift
@@ -14,7 +14,8 @@ class SKActionView: UIView {
     internal var deleteButton: SKDeleteButton!
     internal var shareButton: SKShareButton!
 
-    
+    private var extraMargin: CGFloat = SKMesurement.isPhoneX ? 40 : 0
+
     // Action
     fileprivate var cancelTitle = "Cancel"
     
@@ -29,7 +30,7 @@ class SKActionView: UIView {
     convenience init(frame: CGRect, browser: SKPhotoBrowser) {
         self.init(frame: frame)
         self.frame = frame
-        self.frame = CGRect(x: 0, y: 0, width: frame.width, height: 100)//custom layout
+        self.frame = CGRect(x: 0, y: extraMargin, width: frame.width, height: 100)//custom layout
         self.browser = browser
 
         configureCloseButton()
@@ -49,7 +50,7 @@ class SKActionView: UIView {
     
     func updateFrame(frame: CGRect) {
 //        self.frame = frame
-        self.frame = CGRect(x: 0, y: 0, width: frame.width, height: 100)//custom layout
+        self.frame = CGRect(x: 0, y: extraMargin, width: frame.width, height: 100)//custom layout
         setNeedsDisplay()
     }
 
--- SKPhotoBrowser/SKPaginationView.swift
+++ SKPhotoBrowser/SKPaginationView.swift
@@ -30,7 +30,7 @@ class SKPaginationView: UIView {
     convenience init(frame: CGRect, browser: SKPhotoBrowser?) {
         self.init(frame: frame)
 //        self.frame = CGRect(x: 0, y: frame.height - margin - extraMargin, width: frame.width, height: 100)
-        self.frame = CGRect(x: 0, y: 0, width: frame.width, height: 100 - extraMargin)//custom layout
+        self.frame = CGRect(x: 0, y: extraMargin, width: frame.width, height: 100 - extraMargin)//custom layout
         self.browser = browser
 
         setupApperance()
